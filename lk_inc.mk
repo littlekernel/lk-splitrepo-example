b39159ead7dd6c52d8dddf9e167889e4fef94e11
Travis Geiselbrecht <geist@foobox.com>
2015-09-02 11:40:36 -0700

    [make] update lk_inc.mk with a slightly cleaner version

--- lk_inc.mk
+++ lk_inc.mk
@@ -1,13 +1,19 @@
-# add different top level directories to search for modules
+# copy this and makefile to your external root directory and customize
+# according to how you want to use a split repository
+
+# the top level directory that all paths are relative to
+LKMAKEROOT := .
+
+# paths relative to LKMAKEROOT where additional modules should be searched
 LKINC := projectroot
 
+# the path relative to LKMAKEROOT where the main lk repository lives
+LKROOT := lk
+
 # set the directory relative to LKMAKEROOT where output will go
-#BUILDROOT ?= .
+BUILDROOT ?= .
 
 # set the default project if no args are passed
 DEFAULT_PROJECT ?= testproject
 
-# will probably not want to customize these
-LKMAKEROOT := .
-LKROOT := lk
 #TOOLCHAIN_PREFIX := <relative path to toolchain>
